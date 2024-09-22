afd3d732653e6e2f4d18f0c8530befe7a0794c7b
Muhammet Ali Aygün <109526680+IrohAmca@users.noreply.github.com>
2024-09-22 21:59:27 +0300

    Fixed backward and default utils

--- micrograd.py
+++ micrograd.py
@@ -2,27 +2,32 @@ class Value:
     def __init__(self, data, _children=(), _op='', no_grad=False):
         self.data = data 
         self.grad = 0
-        self._prev = set(_children) if _children else set()
+        self._prev = set(_children)
         self._op = _op
         self._backward = lambda: None
         self.no_grad = no_grad
         self._e = 2.718281828459045
     
     def __call__(self):
-        return "Value(data={}, grad={})".format(self.data, self.grad)
+        return f"Value(data={self.data}, grad={self.grad})"
+    
     def __repr__(self):
         return self.data.__repr__()
+    
+    def __add__(self, value):
+        value = value if isinstance(value, Value) else Value(value)
+        result = Value(self.data + value.data, (self, value), '+')
 
-    def __add__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data + other.data, (self, other), '+')
-
+        if self.no_grad:
+            return result
+        
         def _backward():
-            self.grad += 1.0 * out.grad
-            other.grad += 1.0 * out.grad
-
-        out._backward = _backward
-        return out
+            self.grad += result.grad
+            value.grad += result.grad
+        
+        result._backward = _backward
+        
+        return result
 
 
     def __sub__(self, value):
@@ -39,18 +44,28 @@ class Value:
         result._backward = _backward
         return result
     
-    def __mul__(self, other):
-        other = other if isinstance(other, Value) else Value(other)
-        out = Value(self.data * other.data, (self, other), '*')
+    def __mul__(self, value):
+        value = value if isinstance(value, Value) else Value(value)
+        result = Value(self.data * value.data, (self, value), '*')
+        
+        if self.no_grad:
+            return result
+        
         def _backward():
-            self.grad += other.data * out.grad
-            other.grad += self.data * out.grad
-        out._backward = _backward
-        return out
+            self.grad += value.data * result.grad
+            value.grad += self.data * result.grad
+        result._backward = _backward
+        return result
     
     def __pow__(self, value):
         value = value if isinstance(value, Value) else Value(value)
-        result = Value(self.data ** value.data, (self, value), '**')
+        result = Value(self.data ** value.data, (self, value), 'f**{value}')
+        
+        def _backward():
+            self.grad += (value * self.data**(value-1)) * result.grad
+            
+        result._backward = _backward
+        
         return result
     
     def __truediv__(self, value):
@@ -67,24 +82,32 @@ class Value:
     
     def __sqrt__(self):
         return self ** 0.5
-
-    def __iter__(self):
-        if isinstance(self.data, list):
-            return iter(self.data)
-        else:
-            return iter([self.data])
+        
     def __lt__(self, value):
         value = value if isinstance(value, Value) else Value(value)
         result = Value(self.data < value.data, (self, value), '<')
         return result
-
+    
+    def __le__(self, value):
+        value = value if isinstance(value, Value) else Value(value)
+        result = Value(self.data <= value.data, (self, value), '<=')
+        return result
+    
+    def __eq__(self, value):
+        value = value if isinstance(value, Value) else Value(value)
+        result = Value(self.data == value.data, (self, value), '==')
+        return result
+    
+    def __hash__(self):
+        return hash(id(self))
+    
     def __gt__(self, value):
         value = value if isinstance(value, Value) else Value(value)
         result = Value(self.data > value.data, (self, value), '>')
         return result
     
-    def __radd__(self, other):
-        return self.__add__(other)
+    def __radd__(self, value):
+        return self.__add__(value)
     
     def relu(self):
         result = Value(self.data if self.data > 0 else 0, (self,), 'ReLU')
@@ -94,13 +117,15 @@ class Value:
 
         result._backward = _backward
         return result
-    
+
     def tanh(self):
         e_pos = self._exp()
         e_neg = (-self)._exp()
         result = (e_pos - e_neg) / (e_pos + e_neg)
+        
         def _backward():
             self.grad += (1 - result.data ** 2) * result.grad
+            
         result._backward = _backward
         return result
         
@@ -115,14 +140,7 @@ class Value:
                     build_topo(child)
                 topo.append(v)
 
+        self.grad = 1.0
         build_topo(self)
         for node in reversed(topo):
             node._backward()
-
-
-    @staticmethod
-    def from_list(data):
-        if isinstance(data, list):
-            return [Value(d) for d in data]
-        return Value(data)
-    
